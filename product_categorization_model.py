eb6622236aeaba0785cc7e4f20a87c174d009525
walide67 <walide67@gmail.com>
2019-01-17 21:34:54 +0100

    first commit

---
+++ product_categorization_model.py
@@ -0,0 +1,10 @@
+import numpy
+import pickle
+from keras.preprocessing.text import Tokenizer
+from keras.models import Sequential
+from keras.layers import Activation,Dense,Dropout
+from  TfidfVec import Tfidf_vec as tfIdf
+
+
+
+
